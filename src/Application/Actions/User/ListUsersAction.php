b70e76736e6d8b241088847133473071c22b0c07
okmop1234 <okmop1234@live.com>
2019-10-12 13:00:28 +0700

    restructure to wamp

---
+++ src/Application/Actions/User/ListUsersAction.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Actions\User;
+
+use Psr\Http\Message\ResponseInterface as Response;
+
+class ListUsersAction extends UserAction
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function action(): Response
+    {
+        $users = $this->userRepository->findAll();
+
+        $this->logger->info("Users list was viewed.");
+
+        $this->response->getBody()->write("");
+        return $this->response;
+//        return $this->respondWithData($users);
+    }
+}
